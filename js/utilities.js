b023a560c173ec38a4edbe501b9bb0d04033d001
sajibsaifullah <dusajibphy@gmail.com>
2023-02-14 11:28:01 +0600

    first commit

---
+++ js/utilities.js
@@ -0,0 +1,17 @@
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
+function getTextElementValueById(elementId) {
+    const textElement = document.getElementById(elementId);
+    const textElementValueString = textElement.innerText;
+    const value = parseFloat(textElementValueString);
+    return value;
+}
+function setTextElementValueById(elementId, newValue) {
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
